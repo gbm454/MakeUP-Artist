37a568f29d47bea9de25ffdcaf88b80aa20e17d1
Ganesh BM <128920008+gbm454@users.noreply.github.com>
2024-04-01 15:23:18 +0530

    Updated script.js

--- script.js
+++ script.js
@@ -1,15 +1,21 @@
 var portfolioList = document.querySelectorAll(".portfolio-box");
-portfolioList.forEach(function(portfolioPic){
-    portfolioPic.addEventListener("click", function(){
-        bg = this.style.backgroundImage;
-        document.getElementById("pop_pic_bg").classList.add("active");
-        document.getElementById("pop_pic").classList.add("active");
-        document.getElementById("pop_pic").style.backgroundImage = bg;
-    });
+const description = document.querySelectorAll(".description");
+
+portfolioList.forEach(function (portfolioPic) {
+  portfolioPic.addEventListener("click", function () {
+    var bg = this.style.backgroundImage;
+    var description = this.querySelector(".description").innerText;
+    document.getElementById("pop_pic_bg").classList.add("active");
+    document.getElementById("pop_pic").classList.add("active");
+    document.getElementById("pop_pic").style.backgroundImage = bg;
+    document.getElementById("pop_pic").innerText = description;
+    document.getElementById("pop_pic").style.color = "black";
+    document.getElementById("pop_pic").style.fontSize = "20px";
+    document.getElementById("pop_pic").style.fontWeight = "bold";
+  });
 });
 
-document.getElementById("pop_pic_bg").addEventListener("click", function(){
-    document.getElementById("pop_pic_bg").classList.remove("active");
-    document.getElementById("pop_pic").classList.remove("active");
-
-})
+document.getElementById("pop_pic_bg").addEventListener("click", function () {
+  document.getElementById("pop_pic_bg").classList.remove("active");
+  document.getElementById("pop_pic").classList.remove("active");
+});
